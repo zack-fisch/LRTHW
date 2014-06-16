1d9ed5630c53fa59e165c261dd904c761b916a1c
Zack Fisch <zack@zfisch.com>
2014-06-16 10:58:08 -0400

    last commit, ruby in LRTHW isn't very rubyish. going to explore other resources.

---
+++ ex34.rb
@@ -0,0 +1,7 @@
+animals = ['bear', 'tiger', 'penguin', 'zebra']
+bear = animals[0]
+
+animals = ['bear', 'python', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+puts animals.first(2)
+puts animals.last
